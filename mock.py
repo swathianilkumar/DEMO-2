9707faebeb421dcd2289f806773849da44724b89
swathi <swathianilkumar01@gmail.com>
2025-11-06 12:39:12 +0530

    first commit

---
+++ mock.py
@@ -0,0 +1,34 @@
+# def num(n):
+#     if n<=1:
+#         return n
+#     else:
+#         return num(n-1)+num(n-2)
+# sum=int(input("enter the number:"))
+
+# print("fibonacci series is: ")
+# for i in range(sum):
+#     print(num(i))
+
+
+
+# l=[1,2,3,4]
+# for i in range[-1,-4]:
+#     print(i)
+# n=121
+# if str(n)==str(n[::-1]):
+#     print("p")
+
+
+
+# import math 
+d={"a":1,"b":2, "c":3}
+sum=0
+count=0
+for key in d.values():
+    sum+=key
+    count+=1
+
+avg=sum/count
+print(avg)
+    
+    
